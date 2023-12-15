b60a9e0f970175b42035ce2cd9f444db512bb20b
Jason Sun <sunapi386@gmail.com>
2023-12-14 22:20:14 -0500

    Add dropdowns for start and end positions in Technique Library

--- pages/portal/library.tsx
+++ pages/portal/library.tsx
@@ -1,13 +1,32 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import cytoscape from "cytoscape";
-
-import { Spinner } from "@chakra-ui/react";
+import {
+  Spinner,
+  Box,
+  Select,
+  Heading,
+  Text,
+  VStack,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
 import PortalLayout from "@/components/PortalLayout";
 import { cyElements } from "@/data/cyTransitions";
 
 function Library() {
   const cyRef = useRef(null);
+  const [startPosition, setStartPosition] = useState("");
+  const [endPosition, setEndPosition] = useState("");
+
+  // Mock data for dropdowns, replace with actual data
+  const positions = [
+    "Open Guard",
+    "Closed Guard",
+    "Half Guard",
+    "Side Control",
+    "Mount",
+  ];
 
   useEffect(() => {
     if (!cyRef.current) return;
@@ -19,7 +38,6 @@ function Library() {
         {
           selector: "node",
           style: {
-            "background-color": "#0074D9",
             label: "data(label)",
             "text-valign": "center",
             color: "#fff",
@@ -45,17 +63,58 @@ function Library() {
     });
   }, []);
 
+  const handleSearch = () => {
+    // Logic to filter and display results based on selected positions
+  };
+
   return (
     <PortalLayout>
-      <div>Technique Library</div>
-      {cyRef ? (
-        <div
-          ref={cyRef}
-          style={{ width: "1000px", height: "1000px", background: "white" }}
-        />
-      ) : (
-        <Spinner size="xl" />
-      )}
+      <Box p={6}>
+        <Heading mb={4}>Technique Library</Heading>
+        <VStack spacing={4}>
+          <HStack spacing={4}>
+            <Select
+              placeholder="Start Position"
+              value={startPosition}
+              onChange={(e) => setStartPosition(e.target.value)}
+            >
+              {positions.map((position) => (
+                <option key={position} value={position}>
+                  {position}
+                </option>
+              ))}
+            </Select>
+            <Select
+              placeholder="End Position"
+              value={endPosition}
+              onChange={(e) => setEndPosition(e.target.value)}
+            >
+              {positions.map((position) => (
+                <option key={position} value={position}>
+                  {position}
+                </option>
+              ))}
+            </Select>
+            <Button colorScheme="blue" onClick={handleSearch}>
+              Search
+            </Button>
+          </HStack>
+          <Text>
+            Explore techniques and transitions based on your selected positions.
+            Click on search results to view detailed video clips and images.
+          </Text>
+          {/* Placeholder for search results */}
+          {/* Implement a component or logic to display search results here */}
+        </VStack>
+        {cyRef ? (
+          <Box
+            ref={cyRef}
+            style={{ width: "100%", height: "500px", background: "white" }}
+          />
+        ) : (
+          <Spinner size="xl" />
+        )}
+      </Box>
     </PortalLayout>
   );
 }
